e7bb806d42fe3112f882222ce5d54427b4b92644
Iam Medeiros <iam_medeiros@yahoo.com.br>
2025-03-25 20:59:30 -0300

    implement method renderProductRow - script.js

--- public/js/script.js
+++ public/js/script.js
@@ -20,4 +20,20 @@ class ProductTableRenderer {
         tableHtml += `</tbody></table>`;
         return tableHtml;
     }
+    
+    static renderProductRow(product) {
+        const rowClass = product.hasNegativePrice ? 'negative-price' : '';
+        const actionButton = product.hasEvenNumberInCode 
+            ? `<button onclick="ProductTableRenderer.copyToClipboard(${JSON.stringify(product)})">Copiar JSON</button>`
+            : '';
+        
+        return `
+            <tr class="${rowClass}">
+                <td>${product.name}</td>
+                <td>${product.code}</td>
+                <td>${product.price.toFixed(2)}</td>
+                <td>${actionButton}</td>
+            </tr>
+        `;
+    }
 }
