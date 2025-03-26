32f15a58c7b8f76d2166ed53f53042a82ad9aadb
Iam Medeiros <iam_medeiros@yahoo.com.br>
2025-03-26 09:56:21 -0300

    Refactor index.php

--- public/index.php
+++ public/index.php
@@ -1,2 +1,24 @@
 <?php
-require_once __DIR__ . '/../config/routes.php';
+
+use App\Controllers\ProductController;
+use App\Services\CsvProcessor;
+use App\Services\ProductResponseMapper;
+
+require_once __DIR__ . '/../app/Controllers/ProductController.php';
+require_once __DIR__ . '/../app/Services/CsvProcessor.php';
+require_once __DIR__ . '/../app/Services/ProductResponseMapper.php';
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$csvProcessor = new CsvProcessor(',');
+$responseMapper = new ProductResponseMapper();
+$productController = new ProductController($csvProcessor, $responseMapper);
+
+$action = $_POST['action'] ?? '';
+
+switch ($action) {
+    case 'upload':
+        $productController->upload();
+        break;
+    default:
+        include __DIR__ . '/../app/Views/products.php';
+}
